b5074b20e56fde23041d1639ac7f3ddb97dffa50
merium-rose <87038892+merium-rose@users.noreply.github.com>
2024-11-21 16:20:16 -0500

    main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+#INST326 Section 0203
+#Group: Merium Easterling, Elle Burke, Grace Rippin
+#Assignment: Project 3
+#Date Due: 11/22/2024
+
+#Honor Pledge
+#I pledge that the work contained in this assignment is my own, and that I have complied with University and course policies on academic integrity, and AI use.
+
 def __main__():
     juststarting
